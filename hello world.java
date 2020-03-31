bb30d5bee5eebf40ee7d92217711915247717114
cs123132 <=cs123132.com>
2020-04-01 05:41:16 +0800

    helloworld

---
+++ hello world.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("welcome you");
+        System.out.println("123123");
+        System.out.println("Hello World");
+        
+    }
+}
